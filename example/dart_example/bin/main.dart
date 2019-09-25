a8780ead37287d53010e9c8a3a032b2995346be4
Stalder <serg_lavinov@inbox.ru>
2019-09-25 15:41:45 +0300

    Added example (Dart)

---
+++ example/dart_example/bin/main.dart
@@ -0,0 +1,5 @@
+import '../lib/example.dart';
+
+main(List<String> arguments) async {
+  await run();
+}
---
+++ example/dart_example/lib/example.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:computer/computer.dart';
+
+Future<void> run() async {
+  await Computer().turnOn(areLogsEnebled: true);
+  var a = await Computer().compute<int, int>(fib, param: 40);
+  print('Calculated a: $a');
+  var b = await Computer().compute<int, int>(asyncFib, param: 40);
+  print('Calculated b: $b');
+  var c = await Computer().compute<int, int>(fib, param: 30);
+  print('Calculated c: $c');
+
+  await Computer().turnOff();
+}
+
+int fib(int n) {
+  int number1 = n - 1;
+  int number2 = n - 2;
+
+  if (n == 1) {
+    return 0;
+  } else if (n == 0) {
+    return 1;
+  } else {
+    return (fib(number1) + fib(number2));
+  }
+}
+
+Future<int> asyncFib(int n) async {
+  await Future.delayed(const Duration(seconds: 2));
+
+  int number1 = n - 1;
+  int number2 = n - 2;
+
+  if (n == 1) {
+    return 0;
+  } else if (n == 0) {
+    return 1;
+  } else {
+    return (fib(number1) + fib(number2));
+  }
+}
