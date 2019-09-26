7f06b18de861930fb6ab21dd3a3274ff11f5bd6c
Stalder <serg_lavinov@inbox.ru>
2019-09-26 10:52:43 +0300

    Added something like docs

--- lib/src/computer.dart
+++ lib/src/computer.dart
@@ -8,6 +8,8 @@ import 'package:computer/src/logger.dart';
 import 'task.dart';
 import 'worker.dart';
 
+/// Singleton, that provides compute() like API for concurent calculations
+
 class Computer {
   static Computer _instance = Computer._internal();
 
@@ -21,6 +23,8 @@ class Computer {
   Queue<Task> _taskQueue;
 
   Map<Capability, Completer> _activeTaskCompleters = Map();
+
+  /// Before any computation you need to turn on the Computer
 
   Future<void> turnOn({int workersCount = 2, bool areLogsEnebled}) async {
     if (areLogsEnebled) Logger.enable();
@@ -37,6 +41,8 @@ class Computer {
       Logger.log('Worker $i has started');
     }
   }
+
+  /// Executes function with passed param. Takes only global functions & static methods.
 
   Future<R> compute<P, R>(
     Function fn, {
@@ -70,6 +76,8 @@ class Computer {
     R result = await taskCompleter.future;
     return result;
   }
+
+  /// If you don't need workers anymore, you should turn off the computer
 
   Future<void> turnOff() async {
     Logger.log('Turning off computer...');
